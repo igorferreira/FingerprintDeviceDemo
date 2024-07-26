f869d764db6d60423f1aa933443eec81e1bf6f0b
igorferreira <igorferreirabr@gmail.com>
2024-07-26 03:01:21 -0300

    Commit inicial

---
+++ startgit.sh
@@ -0,0 +1,15 @@
+# Inicializar o repositório localmente
+cd /home/igorferreira/projetos/luiz/AI-JAVA
+git init
+
+# Adicionar todos os arquivos ao Git
+git add .
+
+# Fazer um commit inicial
+git commit -m "Commit inicial"
+
+# Adicionar o repositório remoto
+git remote add origin https://github.com/igorferreira/FingerprintDeviceDemo.git
+
+# Enviar os arquivos para o GitHub
+git push -u origin master
